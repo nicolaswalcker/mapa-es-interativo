8fad10241c6c466d04cfdd98492e5baa5c51d953
nicolaswalcker <walcker_@outlook.com>
2021-12-07 09:31:11 -0300

    feat: adiciona tooltips

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 import "./style.css";
 const cities = document.getElementsByClassName("municipio");
-
+const base = document.createElement("tooltip");
+const fullMap = document.getElementById("svg-map");
 for (let i = 0; i < cities.length; i++) {
-  cities[i].onclick = ()=>{
-    let city = cities[i];
-    city.addEventListener("mouseover", ()=>{
-      
-    })
-    alert(city.getAttribute("name"));
-  }
+  cities[i].addEventListener("click", (e) => {
+    const city = cities[i];
+    /* alert(city.getAttribute("name")); */
+    let text = city.getAttribute("name");
+    let tip = document.createTextNode(text);
+    if ((text = !null)) {
+      base.innerHTML = "";
+      base.appendChild(tip);
+      if (document.getElementsByTagName("tooltip")[0]) {
+        // Checking for any "tooltip" element
+        document.getElementsByTagName("tooltip")[0].remove(); // Removing old tooltip
+      }
+      base.style.top = (e.clientY - 10) + "px";
+      base.style.left = (e.clientX + 15) + "px";
+      document.body.appendChild(base);
+    }
+    fullMap.addEventListener("mouseout", ()=>{
+      if (document.getElementsByTagName("tooltip").length > 0) {
+        document.getElementsByTagName("tooltip")[0].remove();
+      }
+    });
+  });
 }
